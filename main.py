5b6b0e18abebf860f95504797c220055403dacff
AdamF <fornakter@gmail.com>
2023-02-15 14:02:11 +0100

    Client is not allowed to use this service

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import zeep
 from zeep import xsd
-# 4210201242284
 
 client = zeep.Client(wsdl='https://api.mediaexpert.pl/?q=ws/wsdl/products', service_name='TergProductsDataService')
-client2 = zeep.Client(wsdl='https://api.mediaexpert.pl/?q=ws/wsdl/products')
-print(client2.service.testGet(http200=True))
-
-print(client.service.prodsCMS2USPGet(prodID=xsd.SkipValue, prodIndex=xsd.SkipValue, prodEAN='4210201242284',
-                                     insiderFlag=xsd.SkipValue, http200=False))
+try:
+    print(client.service.prodsCMS2USPGet(prodID=xsd.SkipValue, prodIndex=xsd.SkipValue, prodEAN='4210201242284',
+                                         insiderFlag=xsd.SkipValue, http200=False))
+except Exception as e:
+    print(e)
